48128d9fadc1d1c0e4ffc9e3389297e602eb6f15
Tobias Strebitzer <tobias.strebitzer@gmail.com>
2015-07-26 16:57:18 +0800

    fix archive / compress command (file structure)

--- lib/ext/commander/methods.rb
+++ lib/ext/commander/methods.rb
@@ -4,6 +4,5 @@ module Commander
     include MagLove::Helper::CommandHelper
     include MagLove::Helper::ThemeHelper
     include MagLove::Helper::AssetHelper
-    include MagLove::Helper::ArchiveHelper
   end
 end
--- lib/maglove/command/compress.rb
+++ lib/maglove/command/compress.rb
@@ -6,7 +6,27 @@ module MagLove
       def run
         
         task :theme, theme: "!" do |args, options|
-          archive_path("dist/themes/#{options.theme}", "**/*", "#{options.theme}.tar.gz")
+          # archive_path("dist/themes/#{options.theme}", "**/*", "#{options.theme}.tar.gz")
+          target = "themes/#{options.theme}/#{options.theme}.tar.gz"
+          Dir.chdir("dist") do
+            tgz = Zlib::GzipWriter.new(File.open(target, "wb"))
+            Archive::Tar::Minitar::Output.open(tgz) do |tar|
+              Dir["themes/#{options.theme}/**/*"].reject{|file| file == target}.each do |file|
+                Archive::Tar::Minitar::pack_file(file, tar)
+              end
+            end
+          end
+          
+          # Dir.chdir(path) do
+          #   tgz = Zlib::GzipWriter.new(File.open(target, 'wb'))
+          #   Archive::Tar::Minitar::Output.open(tgz) do |tar|
+          #     Dir[pattern].reject{|file| file == target}.each do |file|
+          #       Archive::Tar::Minitar::pack_file(file, tar)
+          #     end
+          #   end
+          # end
+          
+          
           debug("▸ created #{options.theme}.tar.gz")
         end
         
