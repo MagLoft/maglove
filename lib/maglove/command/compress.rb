4950f3b2167382ae108aa0bcfc41fafbf029a5ed
Tobias Strebitzer <tobias.strebitzer@magloft.com>
2016-04-30 14:04:08 +0800

    Remove unnecessary files from Theme archives; Update to 0.6.4

--- lib/maglove/command/compress.rb
+++ lib/maglove/command/compress.rb
@@ -12,20 +12,12 @@ module MagLove
             tgz = Zlib::GzipWriter.new(File.open(target, "wb"))
             Archive::Tar::Minitar::Output.open(tgz) do |tar|
               Dir["themes/#{options.theme}/**/*"].reject{|file| file == target}.each do |file|
-                Archive::Tar::Minitar::pack_file(file, tar)
+                if !(/^themes\/#{options.theme}\/(images|thumbs|templates)/.match(file))
+                  Archive::Tar::Minitar::pack_file(file, tar)
+                end
               end
             end
           end
-          
-          # Dir.chdir(path) do
-          #   tgz = Zlib::GzipWriter.new(File.open(target, 'wb'))
-          #   Archive::Tar::Minitar::Output.open(tgz) do |tar|
-          #     Dir[pattern].reject{|file| file == target}.each do |file|
-          #       Archive::Tar::Minitar::pack_file(file, tar)
-          #     end
-          #   end
-          # end
-          
           
           debug("▸ created #{options.theme}.tar.gz")
         end
